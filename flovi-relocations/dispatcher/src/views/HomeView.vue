6a1b72fa2252de6fe6b03e3eeb420b3cefa6148d
abelardo <abelardo@talent.com>
2026-07-08 10:16:08 +0200

    chore: scaffold vue app + libraries needed + .env.example starting point + git ignore config

---
+++ flovi-relocations/dispatcher/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="flex min-h-screen items-center justify-center">
+    <h1 class="text-3xl font-semibold text-gray-900">Dispatcher App</h1>
+  </main>
+</template>
